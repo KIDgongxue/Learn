b4511ca535fe125f20b8d3fe848469180debbf68
KIDgongxue <16602631127@163.com>
2021-12-12 13:34:55 +0800

    变量

--- src/learn.go/00.hello/main.go
+++ src/learn.go/00.hello/main.go
@@ -4,4 +4,11 @@ import "fmt"
 
 func main()  {
 	fmt.Println("你好，golang")
+	var age, length float64= 12,111
+	fmt.Println("age=" , age, "length=",length)
+	var ageInt, lengthFloat = 12,111.13
+	fmt.Println("ageInt=" , ageInt, "lengthFloat=",lengthFloat)
+	var sex,name = 1,"xiaoli"
+	fmt.Println("name=",name,"sex=",sex)
+
 }
