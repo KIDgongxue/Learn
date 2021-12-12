8807085c9a563e66f14b7a494cd5101aa410fbb7
KIDgongxue <16602631127@163.com>
2021-12-12 16:26:48 +0800

    计算

--- src/learn.go/00.hello/main.go
+++ src/learn.go/00.hello/main.go
@@ -10,5 +10,7 @@ func main()  {
 	fmt.Println("ageInt=" , ageInt, "lengthFloat=",lengthFloat)
 	var sex,name = 1,"xiaoli"
 	fmt.Println("name=",name,"sex=",sex)
+	var a,b int = 30,10 // 如为int8，会造成溢出
+	fmt.Println(a*b)
 
 }
